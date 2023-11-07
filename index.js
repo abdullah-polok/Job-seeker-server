1ae00c388e05c278628ec09b406f34db5e8ef25f
Abdullah's PC <abdullahalrahman23@gmail.com>
2023-11-07 11:15:48 +0300

    added specific user data access

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+
+///require jwt
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 ///middleware
 app.use(express.json())
-app.use(cors());
-
+app.use(cors({
+    origin: [
+        'http://localhost:5173', 'http://localhost:5174'
+    ],
+    credentials: true
+}));
+app.use(cookieParser())
 
 
 
@@ -23,6 +32,36 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+///custom middle ware
+const logger = (req, res, next) => {
+    console.log('log:info', req.method, req.url);
+    next()
+}
+
+///use for verify the user
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token
+    // console.log('token in the middleware', token)
+
+    ///if dont have token then use this
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+
+    //verify jwt token
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+
+        req.user = decoded
+        next();
+    })
+}
+
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -33,6 +72,46 @@ async function run() {
         const alltypeCollection = client.db('jobseeker').collection('jobtype')
         const bidsCollection = client.db('jobseeker').collection('bids')
         const addJobCollection = client.db('jobseeker').collection('addjobs')
+
+
+        ////auth related api fro jwt 
+        /// when user log in
+        app.post('/jwt', logger, async (req, res) => {
+            const user = req.body
+            console.log(user)
+
+            ///payload
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            ///set cookie
+            //when user login set cookie into local storage cookie in application
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+
+            })
+            res.send({ success: true })
+        })
+
+
+        //// when user log out remove the cookie for safety
+        app.post('/logout', async (req, res) => {
+
+            const user = req.body;
+
+            //for seeing user log out
+            console.log('logging out', user);
+
+            ///clear the cookie after log out
+            ///maxAge : 0 no need to save into the localstorage
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+        })
+
+
+
+
+
         ///get all type of job
         app.get('/alltype', async (req, res) => {
             const cursor = alltypeCollection.find();
@@ -66,8 +145,21 @@ async function run() {
         })
 
         ////get bids data into the database
-        app.get('/bids', async (req, res) => {
-            const cursor = bidsCollection.find();
+        app.get('/bids', logger, verifyToken, async (req, res) => {
+            ///check cookies data
+            // console.log("Cookies :", req.cookies)
+
+            ///check exact user email then provide tha data 
+            ///if email not match with the user email then it will not show data
+            if (req.user.email !== req.query.email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
+            }
+
+            const cursor = bidsCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
         })
